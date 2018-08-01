d4b0f2764d247975160f1723fe46b6d4bf48dd9e
infinitydev4 <infinitydev4@gmail.com>
2018-07-31 18:21:20 -0700

    enfin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "je_me_presente_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "premier_site_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
